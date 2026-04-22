21be2249c7734aa1f0e9c41b0037cbaddf4fe370
Adam <adam.alaouiti@gmail.com>
2026-04-22 17:00:38 +0200

    feat: enable CORS for client requests

--- dotnet-core/CatMash.Api/Program.cs
+++ dotnet-core/CatMash.Api/Program.cs
@@ -5,7 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<CatService>();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowCatMashClient",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowCatMashClient");
 
 app.UseHttpsRedirection();
 
